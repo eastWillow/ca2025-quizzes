f5b7bfd5a4d61dfc358a570ce8f8def9ef99a9ec
eastWillow <bobgash2@gmail.com>
2025-11-09 00:00:45 +0800

    feat(env): add get ticks functions

--- q3-problemC/q3-problemC.c
+++ q3-problemC/q3-problemC.c
@@ -22,6 +22,24 @@ void *memcpy(void *dest, const void *src, unsigned long n)
     return dest;
 }
 
+typedef uint64_t ticks;
+static inline ticks getticks(void)
+{
+    uint64_t result;
+    uint32_t l, h, h2;
+    asm volatile(
+        "rdcycleh %0\n"
+        "rdcycle %1\n"
+        "rdcycleh %2\n"
+        "sub %0, %0, %2\n"
+        "seqz %0, %0\n"
+        "sub %0, zero, %0\n"
+        "and %1, %1, %0\n"
+        : "=r"(h), "=r"(l), "=r"(h2));
+    result = (((uint64_t) h) << 32) | ((uint64_t) l);
+    return result;
+}
+
 static uint64_t mul32(uint32_t a, uint32_t b)
 {
     uint64_t r = 0;
